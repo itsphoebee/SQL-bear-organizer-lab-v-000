775382c2d898df6816744b4b393ace0ed463acd4
Phoebe Lau <phoebelau7@gmail.com>
2017-12-26 22:37:42 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,9 @@
-INSERT INTO bears (name, age, gender, color,temperament, alive) VALUES 
-  ("Mr.Chocolate", 16, 'M', "brown", "happy", true),
-  ("Mr.Chocolate", 16, 'M', "brown", "happy", true),
-  ("Mr.Chocolate", 16, 'M', "brown", "happy", true),
-  ("Mr.Chocolate", 16, 'M', "brown", "happy", true),
-  ("Mr.Chocolate", 16, 'M', "brown", "happy", true),
-  ("Mr.Chocolate", 16, 'M', "brown", "happy", true),
-  ("Mr.Chocolate", 16, 'M', "brown", "happy", true),
-  ("Mr.Chocolate", 16, 'M', "brown", "happy", true);
-
-
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+  ("Mr.Chocolate", 10, 'M', "brown", "happy", true),
+  ("Rowdy", 9, 'M', "red", "angry", true),
+  ("Tabitha", 4, 'F', "brown", "sleepy", true),
+  ("Sergeant Brown", 12, 'M', "brown", "solemn", true),
+  ("Melissa", 3, 'F', "black", "happy", false),
+  ("Grinch", 15, 'M', "brown", "happy", true),
+  ("Wendy", 1, 'M', "brown", "happy", true),
+  (NULL, 7, 'M', "brown", "happy", true);
